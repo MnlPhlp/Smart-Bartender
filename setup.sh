f8d7da0d0d613bc58cffc911ee1e7b210e67d7eb
MnlPhlp <mail@manuel.sh>
2021-06-24 13:17:45 +0200

    fixed setup

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo cloning repository
 git clone https://github.com/MnlPhlp/Smart-Bartender.git
 cd Smart-Bartender
 echo creating venv
-python3 -m venv .
-source ./bin/activate
+python3 -m venv venv
+source ./venv/bin/activate
 echo installing libraries
 pip install -r requirements.txt
